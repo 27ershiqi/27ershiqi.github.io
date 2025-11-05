86a437fb28a954c44f6278d7e779c9e2a4b76515
27ershiqi <1460277794@qq.com>
2025-11-05 10:01:26 +0800

    直接部署构建文件到根目录

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './index.css'
-import './assets/highlight.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./index.css";
+import "./assets/highlight.css";
 
-console.log('开始初始化 Vue 应用...')
+console.log("开始初始化 Vue 应用...");
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
 
-console.log('应用初始化完成')
+console.log("应用初始化完成");
