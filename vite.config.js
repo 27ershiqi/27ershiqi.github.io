80854dbd6bfa87753419c0ae3cd8935298f3cd79
27ershiqi <1460277794@qq.com>
2025-11-06 16:04:36 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [vue()],
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
-    },
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    }
   },
 });
