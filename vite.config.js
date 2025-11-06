53fda08b58c7497721fe0a48fc902d067eb9286d
27ershiqi <1460277794@qq.com>
2025-11-06 16:50:26 +0800

    自定义打包配置

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import { setupBuild } from './build/index';
 
 export default defineConfig({
+  build: setupBuild(),
   plugins: [vue()],
   base: process.env.NODE_ENV === "production" ? "./" : "/",
   build: {
@@ -10,6 +12,7 @@ export default defineConfig({
     assetsDir: "assets",
     rollupOptions: {
       output: {
+        sanitizeFileNames: false, // 禁用文件名清理
         manualChunks: undefined, // 禁用代码分割
         chunkFileNames: "assets/[name]-[hash].js",
         entryFileNames: "assets/[name]-[hash].js",
