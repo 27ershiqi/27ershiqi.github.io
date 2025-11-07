c8cf96c9e0a758bb4014ef0842c12327dfab8382
27ershiqi <1460277794@qq.com>
2025-11-07 10:31:35 +0800

    修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "./" : "/",
+  base: "/dist/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
